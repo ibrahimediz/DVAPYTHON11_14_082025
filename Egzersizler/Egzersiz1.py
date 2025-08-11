aaefc2c83ebc6f901aa66f66dc79beb52fbca54c
İbrahim EDİZ <edizdesign@gmail.com>
2025-08-11 12:59:20 +0300

    11082025_1

---
+++ Egzersizler/Egzersiz1.py
@@ -0,0 +1,5 @@
+"""
+Marka,motor,yıl ve model bilgisinin yer aldığı bir araba sınıf yazalım.
+bu sınıf üzerinden 2 araba tanımı yapıp sınıf içerisinde yer alacak olan 
+bilgiver fonkisyonunu çalıştırarak arabaya ait olan özellikleri ekrana yazdıralım
+"""
