88d9e711247a82e989d028394e97c2f97b9d9969
İbrahim EDİZ <edizdesign@gmail.com>
2025-08-11 12:30:41 +0000

    11082025_6

--- Egzersizler/muge/egzersiz2_muge.py
+++ Egzersizler/muge/egzersiz2_muge.py
@@ -1,5 +1,32 @@
 """
 diagram1.png dosyasını dikkate alarak 4 class oluşturalım.
 """
+class kara_araclari:
+    def __init__(self, marka, model, yil):
+        self.marka = marka
+        self.model = model
+        self.yil = yil
+    def bilgi_ver(self):
+        print(f"Marka:{self.marka}, Model:{self.model}, Yil:{self.yil}")
 
-class 
+class Araba(kara_araclari):
+    def __init__(self, marka, model, yil):
+        super().__init__(marka, model, yil)
+        self.motor = motor
+    def bilgi_ver(self):
+        super().bilgi_ver
+        print(f"Motor:{self.motor}")
+class Otobus(kara_araclari):
+    def __init__(self, marka, model, yil):
+        super().__init__(marka, model, yil)
+        self.motor = motor
+    def bilgi_ver(self):
+        super().bilgi_ver
+        print(f"Motor:{self.motor}")
+class Minibus(kara_araclari):
+    def __init__(self, marka, model, yil):
+        super().__init__(marka, model, yil)
+        self.motor = motor
+    def bilgi_ver(self):
+        super().bilgi_ver
+        print(f"Motor:{self.motor}")
